6b624b39880e2a59cda27ff137d0ebdc2fec1565
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-08-28 08:39:07 -0300

    fix: cambiar lector de segundos a lector de milisegundos

--- pomodoro-frontend/src/logic/readSecondsTimer.ts
+++ pomodoro-frontend/src/logic/readSecondsTimer.ts
@@ -1,6 +1,8 @@
 import type { Timer } from "../models/timer"
+import { msToSec } from "./minToMilisec";
 
-export const readSecondsTimer = (sec : number): Timer => {
+export const readSecondsTimer = (ms : number): Timer => {
+    const sec = msToSec(ms);
     const minutes = Math.floor(sec/60);
     const seconds = sec%60;
     return{minutes,seconds}
