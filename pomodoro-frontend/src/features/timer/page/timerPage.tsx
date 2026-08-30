8363206f558ac36e67c1388058500f6275fc4c4b
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-08-30 20:12:45 -0300

    feat: Añadir botones de Resumir y Parar, junto a funcionalidad para parar el temporizador

--- pomodoro-frontend/src/features/timer/page/timerPage.tsx
+++ pomodoro-frontend/src/features/timer/page/timerPage.tsx
@@ -3,6 +3,7 @@ import DisplayTimer from "../displayTimer";
 import { readSecondsTimer } from "../../../logic/readSecondsTimer";
 import { calcFinishTime, calcTimeMs } from "../../../logic/calcTime";
 import { minToMs } from "../../../logic/minToMilisec";
+import type { TimerState } from "../../../models/timerState";
 import styles from "./timerPage.module.css"
 
 var WORK_MINUTES = 1;
@@ -10,7 +11,8 @@ var WORK_MINUTES = 1;
 const TimerPage = () => {
     const [finishTime, setFinishTime] = useState<number | null>(null);
     const [isRunning, setIsRunning] = useState(false);
-    const [timer, setTimer] = useState(readSecondsTimer(0));
+    const [timerState, setTimerState] = useState<TimerState>("stop");
+    const [timer, setTimer] = useState(readSecondsTimer(minToMs(10)));
 
     useEffect(() => {
         if (!isRunning || finishTime === null) return;
@@ -21,6 +23,7 @@ const TimerPage = () => {
             if (remaining <= 0) {
                 setTimer(readSecondsTimer(0));
                 setIsRunning(false);
+                setTimerState("stop");
                 return;
             }
 
@@ -33,6 +36,7 @@ const TimerPage = () => {
     const start = () => {
         setFinishTime(calcFinishTime(WORK_MINUTES));
         setIsRunning(true);
+        setTimerState("running");
     };
 
     const setTime = (e:number) => {
@@ -40,10 +44,31 @@ const TimerPage = () => {
         setTimer(readSecondsTimer(minToMs(e)));
     };
 
+    const hideButtons = (tS : TimerState, initial:boolean) => {
+        if(initial){
+            if(tS === "running" || tS === "pause"){
+            return styles.hide;
+            }
+            return "";
+        }else{
+            if(tS === "running" || tS === "pause"){
+            return "";
+            }
+            return styles.hide;
+        }
+    }
+
+    const stopTimer = () =>{
+        setTimer(readSecondsTimer(minToMs(10)));
+        setIsRunning(false);
+        setTimerState("stop");
+        return;
+    }
+
     return (
         <section className={styles.timerSection}>
             <DisplayTimer timer={timer}/>
-            <div className={styles.timerTypesContainer}>
+            <div className={`${styles.timerTypesContainer} ${hideButtons(timerState, true)}`}>
                 <p className={styles.timerTypesTitle}>Tiempo De Sesión:</p>
                 <div className={styles.timeButtonContainer}>
                     <button className={styles.timeButton} onClick={() => setTime(40)} disabled={isRunning}>40min</button>
@@ -51,7 +76,13 @@ const TimerPage = () => {
                     <button className={styles.timeButton} onClick={() => setTime(10)} disabled={isRunning}>10min</button>
                 </div>
             </div>
-            <button className={styles.startButton} onClick={start} disabled={isRunning}>Iniciar</button>
+            <div className={styles.buttonContainer}>
+                <button className={`${styles.startButton}  ${hideButtons(timerState, true)}`} onClick={start} disabled={isRunning}>Iniciar</button>
+                <div className={`${styles.pauseButtonsContainer} ${hideButtons(timerState, false)}`}>
+                    <button className={styles.pauseButton} disabled={!isRunning}>Resumir</button>
+                    <button className={styles.stopButton} disabled={!isRunning} onClick={stopTimer}>X</button>
+                </div>
+            </div>
         </section>
     );
 };
