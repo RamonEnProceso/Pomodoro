212c59ef942e57675b6cdd7ec320ce3ca2fa0b3d
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-08-30 21:18:42 -0300

    fix: Eliminar función redundante

--- pomodoro-frontend/src/logic/calcTime.ts
+++ pomodoro-frontend/src/logic/calcTime.ts
@@ -1,20 +1,13 @@
-import { minToMs } from "./minToMilisec";
-
 export const timeNow = () : number => {
     return Date.now();
 }
 
 export const calcFinishTime = (min:number) : number => {
     const startTime = timeNow();
-    const finishTime = startTime + minToMs(min);
+    const finishTime = startTime + min;
     return finishTime;
 }
 
 export const calcTimeMs = (finishTime:number) : number => {
     return finishTime - timeNow();
-}
-
-export const calcNewFinishTime = (pauseTime:number,finishTime:number) : number => {
-    const pendingTime = finishTime - pauseTime;
-    return timeNow() + pendingTime;
 }
